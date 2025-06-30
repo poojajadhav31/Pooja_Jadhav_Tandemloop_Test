1815e5d22b89e54c4e73f6a57a8c3fb961bee021
pooja Jadhav <157224228+poojajadhav31@users.noreply.github.com>
2025-06-30 18:31:44 +0530

    Update Problem-1.py

--- Problem-1.py
+++ Problem-1.py
@@ -15,7 +15,6 @@ class Calculator:
         else:
             return "Invalid Operation"
 
-# User interaction
 print("Simple Calculator")
 a = float(input("Enter first number (a): "))
 b = float(input("Enter second number (b): "))
